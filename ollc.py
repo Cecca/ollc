7b5cd9ee00333f5df73800649c06472fa9e4f1f0
Matteo Ceccarello <ceccarel@dei.unipd.it>
2015-10-22 19:06:43 +0200

    Add some documentation

--- ollc.py
+++ ollc.py
@@ -1,29 +1,52 @@
 #!/usr/bin/env python
 
-## Cross-version stuff
+## Cross-version imports. This allows to write python3-like code that
+## can be run by a python2 interpreter.
+## See http://python-future.org/imports.html
 from __future__ import absolute_import, division, print_function
-## FIXME see how to import this
-#from builtins import *
-
-import subprocess
-
-try:
-    from subprocess import DEVNULL # py3k
-except ImportError:
-    import os
-    DEVNULL = open(os.devnull, 'wb')
 
 import os
 import sys
-## TODO: Investigate YAML (needs additional dependency)
+import subprocess
 import json
 
+
+## subprocess.DEVNULL is defined only in python3. If we fail to import it
+## (i.e. we are on python2) we define it manually.
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    DEVNULL = open(os.devnull, 'wb')
+
+## This is a mapping between command line options and the name with which they
+## are saved in the JSON file. This allows to automatically set options from
+## both the command line and the saved JSON configuration.
 OPTIONS = {'--rev': 'openlilylib_revision'}
 
 class OpenLilyLibRepo(object):
+    """The (local) repository of OpenLilyLib code.
+
+    This class represents the local mirror of the upstream OpenLilyLib
+    repository. It provides facilities to clone from upstream, pull changes and
+    checkout specific revisions.
+
+    To create a new object of this class use, for instance
+
+        OpenLilyLibRepo.at_revision('master')
+    """
 
     @staticmethod
     def at_revision(revision):
+        """Instantiate a repository class at the specified revision.
+
+        If the repository does not exist it is cloned. If the revision is
+        already present in the repository, it is checked out. If the repository
+        does not contain the revision, the latest changes are pulled from
+        upstream.
+
+        Branch names are considered valid as arguments to the function: they
+        will always trigger a pull from upstream.
+        """
         repo = OpenLilyLibRepo()
         if not repo.has_revision(revision):
             print("Updating OpenLilyLib repository information")
@@ -41,6 +64,9 @@ class OpenLilyLibRepo(object):
         self.remote_repo = 'https://github.com/openlilylib/openlilylib.git'
 
     def git(self, args, capture_output=False):
+        """Runs git on the local OpenLilyLib clone. Set capture_output to True to
+        have the output of the git command returned as a string
+        """
         command = ['git']
         command.append('--work-tree={}'.format(self.local_repo))
         command.append('--git-dir={}'.format(self.local_git_dir))
@@ -60,6 +86,8 @@ class OpenLilyLibRepo(object):
             return output
 
     def has_revision(self, revision):
+        """Return true if the given revision is in the `rev-list` and
+        the repository exists."""
         if not self.has_clone():
             return False
         revlist = self.git(['rev-list', '--all'], capture_output=True)\
@@ -67,9 +95,11 @@ class OpenLilyLibRepo(object):
         return revision in revlist
 
     def has_clone(self):
+        """Return true if the repository has been cloned."""
         return os.path.isdir(self.local_repo)
 
     def clone(self):
+        """Clones the repository from github."""
         print("Cloning the repository")
         subprocess.call(['git',
                          'clone',
@@ -78,25 +108,31 @@ class OpenLilyLibRepo(object):
                          self.local_repo])
 
     def clone_if_needed(self):
+        """Clones the repository only if needed."""
         if not self.has_clone():
             self.clone()
 
     def pull(self):
+        """Pull the latest changes from upstream."""
         self.git(['pull'])
 
     def checkout(self, revision):
+        """Checkout the given revision"""
         self.git(['checkout', revision])
 
     def current_revision(self):
+        """Returns the current revision as given by `git rev-parse`"""
         return self.git(['rev-parse', 'HEAD'],
                         capture_output=True).decode().strip()
 
     def include_dirs(self):
+        """Return the include directories to be added to lilypond's path."""
         return [self.local_repo,
                 os.path.join(self.local_repo, 'ly')]
 
 
 class LilypondCommand(object):
+    """Wrapper for the lilypond command."""
 
     def __init__(self, config):
         self.oll_revision = config['openlilylib_revision']
@@ -107,10 +143,24 @@ class LilypondCommand(object):
 
     @staticmethod
     def config_file():
+        """The path to the configuration file in the local directory.
+
+        Returns the name of the configuration file in the directory in which
+        this script is executed
+        """
         return os.path.join(os.curdir, '.ollc.json')
 
     @staticmethod
     def load_config(arglist):
+        """Loads the configuration for this command.
+
+        In order of precedence:
+
+         - Command line
+         - Configuration file
+         - Defaults
+        """
+
         # Define defaults
         config = {
             'openlilylib_revision': 'master'
@@ -133,10 +183,12 @@ class LilypondCommand(object):
 
     @staticmethod
     def from_config(arglist):
+        """Factory method to create a command from the given argument list."""
         config = LilypondCommand.load_config(arglist)
         return LilypondCommand(config)
 
     def persist_config(self):
+        """Save the current command configuration to 'self.config_file()'"""
         with open(LilypondCommand.config_file(), 'w') as out:
             json.dump({
                 'openlilylib_revision': self.oll_revision
@@ -144,6 +196,11 @@ class LilypondCommand(object):
 
     @staticmethod
     def filter_lily_args(arglist):
+        """Filter the given argument list, retaining only the arguments to
+        be passed to Lilypond.
+
+        Removes options declared in OPTIONS (along with their arguments).
+        """
         filtered = list(arglist)
         for opt in OPTIONS.keys():
             if opt in filtered:
@@ -156,7 +213,8 @@ class LilypondCommand(object):
         return filtered
 
     def run(self, args):
-        ## FIXME make lilypond version configurable
+        """Runs lilypond with the given arguments, adding to the include
+        path the relevant directories of the OpenLilyLib repository."""
         command = ['lilypond']
         for include_dir in self.oll_repo.include_dirs():
             command.extend(['-I', include_dir])
